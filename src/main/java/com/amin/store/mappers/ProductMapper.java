945353463fee6231873ee726a2fa0ed53ee0f209
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-10-21 17:29:36 +0330

    Building product endpoints

---
+++ src/main/java/com/amin/store/mappers/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.amin.store.mappers;
+
+import com.amin.store.dtos.ProductDto;
+import com.amin.store.entities.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(target = "categoryId", source = "category.id")
+    ProductDto toDto(Product product);
+}
