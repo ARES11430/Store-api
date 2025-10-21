44c644bc789199de5cb36f61f40b6f95064aee99
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-10-21 14:43:50 +0330

    Use DTO for users API.

--- src/main/java/com/amin/store/controllers/UserController.java
+++ src/main/java/com/amin/store/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.amin.store.controllers;
 
+import com.amin.store.dtos.UserDto;
 import com.amin.store.entities.User;
 import com.amin.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -13,16 +16,19 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public Iterable<User> getUsers() {
-        return userRepository.findAll();
+    public List<UserDto> getUsers() {
+        return userRepository.findAll().stream()
+                .map(user -> new UserDto(user.getId(),user.getName(),user.getEmail())).toList();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable long id) {
+    public ResponseEntity<UserDto> getUserById(@PathVariable long id) {
        var user = userRepository.findById(id).orElse(null);
        if (user == null) {
             return ResponseEntity.notFound().build();
        }
-       return ResponseEntity.ok(user);
+
+       var userDto = new UserDto(user.getId(),user.getName(),user.getEmail());
+       return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/amin/store/repositories/UserRepository.java
+++ src/main/java/com/amin/store/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.amin.store.repositories;
 
 import com.amin.store.entities.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
