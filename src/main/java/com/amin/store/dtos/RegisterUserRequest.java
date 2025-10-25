a59c7b058347942ae874a4fa685e073396691de3
Amin Ghasemi <amin11.ghasemi@gmail.com>
2025-10-25 10:12:51 +0330

    Installing Jakarta validation.

--- src/main/java/com/amin/store/dtos/RegisterUserRequest.java
+++ src/main/java/com/amin/store/dtos/RegisterUserRequest.java
@@ -1,10 +1,19 @@
 package com.amin.store.dtos;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterUserRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 255, message = "Name must be less than 255 characters")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 25, message = "Password must be between 6 to 25 characters long")
     private String password;
 }
